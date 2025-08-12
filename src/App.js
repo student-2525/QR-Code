1734f0e25809175f450e9a4c5b0eac3b15492cb4
student <mehwishkhan2115@gmail.com>
2025-08-12 13:13:53 +0530

    Add commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import QRCode from "./App.css";
+import { useState } from "react";
+
 function App() {
+const[url, setUrl] = useState('')
+const[qrcode, setQrcode] = useState('')
+const GenerateQRCode = () => {
+  QRCode.toDataURL(url, (err, urL) => {
+    if (err) return console.error(err)
+
+      console.log(urL)
+    setQrcode(urL)
+  })
+}
+
   return (
     <div className="App">
-     <h1>Hello World!</h1>
+     <h1>QR Code Generator</h1>
+     <input
+     type="text"
+     placeholder="e.g. https://google.com"
+     value={url}
+     onChange={(evt) => setUrl(evt.target.value)} />
+     <button onClick={GenerateQRCode}>Generate</button>
+     <img src={qrcode} />
     </div>
   );
 }
